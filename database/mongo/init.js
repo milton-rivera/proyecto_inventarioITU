c88089a910336356e6c4da88ae90285ebb4acb2e
Luciano Papagni <lucianopapagni123@gmail.com>
2026-06-29 16:06:36 -0300

    dbmongo

---
+++ database/mongo/init.js
@@ -0,0 +1,65 @@
+[
+  {
+    "_id": 1,
+    "tipo": "Desktop",
+    "fabricante": "Dell",
+    "modelo": "OptiPlex 7090",
+    "sistema_operativo": "Windows 10 Pro",
+    "componentes": {
+      "cpu": "Intel Core i7-10700",
+      "ram": "16GB DDR4",
+      "disco": "512GB SSD NVMe"
+    },
+    "perifericos": {
+      "monitor": "Dell P2419H 24 pulgadas",
+      "mouse": "Dell Optical Mouse",
+      "teclado": "Dell Multimedia Keyboard"
+    }
+  },
+  {
+    "_id": 2,
+    "tipo": "Laptop",
+    "fabricante": "Lenovo",
+    "modelo": "ThinkPad T14",
+    "sistema_operativo": "Ubuntu 22.04 LTS",
+    "componentes": {
+      "cpu": "AMD Ryzen 5 PRO 4650U",
+      "ram": "16GB DDR4",
+      "disco": "256GB SSD"
+    }
+  },
+  {
+    "_id": 3,
+    "tipo": "Desktop",
+    "fabricante": "HP",
+    "modelo": "ProDesk 400 G7",
+    "sistema_operativo": "Windows 11 Pro",
+    "componentes": {
+      "cpu": "Intel Core i5-10500",
+      "ram": "8GB DDR4",
+      "disco": "1TB HDD"
+    },
+    "perifericos": {
+      "monitor": "HP V22v 21.5 pulgadas",
+      "mouse": "HP USB Optical Mouse",
+      "teclado": "HP USB Keyboard"
+    }
+  },
+  {
+    "_id": 4,
+    "tipo": "Desktop",
+    "fabricante": "Lenovo",
+    "modelo": "ThinkCentre M70q",
+    "sistema_operativo": "Windows 10 Pro",
+    "componentes": {
+      "cpu": "Intel Core i5-11400T",
+      "ram": "16GB DDR4",
+      "disco": "256GB SSD NVMe"
+    },
+    "perifericos": {
+      "monitor": "Lenovo ThinkVision 24 pulgadas",
+      "mouse": "Lenovo Calliope Mouse",
+      "teclado": "Lenovo Traditional Keyboard"
+    }
+  }
+]
